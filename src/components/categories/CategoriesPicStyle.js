0558a35b15528d7c9603503a884fdb50f39e459d
besansaid <besanhosnysaid203998@gmail.com>
2022-08-02 13:16:29 +0300

    finish header section

---
+++ src/components/categories/CategoriesPicStyle.js
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const ImageList = styled.ul`
+    list-style: none;
+    padding: 10px 0;
+    display: flex;
+    justify-content: space-between;
+    color: #fff;
+    font-weight: 600;
+    margin: -50px;
+    li{
+        text-transform: capitalize;
+    }
+`
