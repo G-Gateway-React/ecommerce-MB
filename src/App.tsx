1738b84ef14af6a718c57508016d97cd0e6695ef
Yasmeen <yasmeentayeh7@gmail.com>
2022-08-16 10:32:49 +0300

    Edit Product Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Home from './pages/home/Home'
 import CssBaseline from '@mui/material/CssBaseline';
 import { Global, css } from "@emotion/react";
+import Product from "./pages/Product/Product";
 
 
 const App = () => {
   return (
     <>
       <CssBaseline />
-      <Home />
+      {/*<Home />*/}
+      <Product/>
+
     </>
   );
 }
