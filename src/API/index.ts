10091a8d02dbe3556653ab9f88feac47ea8530cd
Yasmeen <yasmeentayeh7@gmail.com>
2022-09-18 10:47:40 +0300

    connect category with api

--- src/API/index.ts
+++ src/API/index.ts
@@ -1,1 +1,4 @@
 export const baseUrl= "https://pro-commerce1.herokuapp.com/api/v1"
+
+export const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1vc3NhMTIzQGdtYWlsLmNvbSIsIm5hbWUiOiJiaWdzYW0iLCJpYXQiOjE2NjMxNDAxNjl9.WMLMav7L4dlUOjfYfKQx7_JeGcIHP4aRaSX2TlLfCQY";
